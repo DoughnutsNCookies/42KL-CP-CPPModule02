a253a874523b5f206926a8ef0615a71fb600a785
Chuah Tse Yung <105269435+DoughnutsNCookies@users.noreply.github.com>
2022-08-21 13:04:09 +0800

    Update comment

--- CPPModule02/ex03/srcs/Fixed.cpp
+++ CPPModule02/ex03/srcs/Fixed.cpp
@@ -45,7 +45,7 @@ Fixed::~Fixed()
 
 }
 
-/* Copy Assignation operator function of Fixed class */
+/* Copy assignation operator function of Fixed class */
 Fixed	&Fixed::operator=(const Fixed &src)
 {
 	this->_number = src.getRawBits();
