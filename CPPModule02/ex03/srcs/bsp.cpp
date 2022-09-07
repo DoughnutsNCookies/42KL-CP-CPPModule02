218723af01011a2cc1ddd5b1c67ab706b50b7c84
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2022-09-07 19:27:12 +0800

    Fix find triangle formula

--- CPPModule02/ex03/srcs/bsp.cpp
+++ CPPModule02/ex03/srcs/bsp.cpp
@@ -6,13 +6,13 @@
 /*   By: schuah <schuah@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 09:46:10 by schuah            #+#    #+#             */
-/*   Updated: 2022/08/21 10:51:37 by schuah           ###   ########.fr       */
+/*   Updated: 2022/09/07 19:25:35 by schuah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-/* Area of a triangle: [Ax(By - Cy) + Bx(Cy - Ay) + Cx(By - Ax)] / 2
+/* Area of a triangle: [Ax(By - Cy) + Bx(Cy - Ay) + Cx(Ay - By)] / 2
 ** Return absolute value because area cannot be <= 0 */
 static float get_area(Point const a, Point const b, Point const c)
 {
@@ -20,7 +20,7 @@ static float get_area(Point const a, Point const b, Point const c)
 
 	area = ((a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()))
 			+ (b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat()))
-			+ (c.getX().toFloat() * (b.getY().toFloat() - a.getX().toFloat()))) / 2;
+			+ (c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat()))) / 2;
 	return (area >= 0 ? area : (area * -1));
 }
 
@@ -35,7 +35,7 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	t_pbc = get_area(point, b, c);
 	t_pac = get_area(point, a, c);
 	if (a.getX() == 0 && a.getY() == 0 && b.getX() == 0 && b.getY() == 0
-		&& c.getX() == 0 && c.getY() == 0 && point.getX() != 0 && point.getY() != 0)
+		&& c.getX() == 0 && c.getY() == 0 && (point.getX() != 0 || point.getY() != 0))
 			return (false);
 	return (t_pab + t_pbc + t_pac == t_abc ? true : false);
 }
